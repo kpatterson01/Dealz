69d4ca6ef9fe285277b2f410b9a0f46c8e220aa9
kpatterson01 <41876222+kpatterson01@users.noreply.github.com>
2021-04-10 15:47:43 -0400

    Edit Log In Screen

--- Dealz/ViewController.swift
+++ Dealz/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var logIn: UIButton!
+    
+    @IBOutlet weak var signUp: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        logIn.layer.cornerRadius = 20
+        signUp.layer.cornerRadius = 20
     }
     
    // override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
